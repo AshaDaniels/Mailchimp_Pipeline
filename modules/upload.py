11507d61d51ac41e7fadf705a20c50c1466071bd
AshaDaniels <asha.m.daniels@gmail.com>
2025-06-03 09:54:13 +0100

    Updated credentials for Kestra
    Capitalised values

--- modules/upload.py
+++ modules/upload.py
@@ -2,20 +2,20 @@ import boto3
 from dotenv import load_dotenv
 from os.path import isfile, join
 
-def upload_data(aws_access_key_id, aws_secret_access_key, file_name, bucket_name, bucket_file_path):
+def upload_data(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, FILE_NAME, BUCKET_NAME, BUCKET_FILE_PATH):
 
     # Create an S3 Client using AWS Credentials
     s3_client = boto3.client(
         's3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
 
     print('s3_client created')
 
     # Upload file (Key) to S3 Bucket
     s3_client.upload_file(
-        Filename = file_name,
-        Bucket = bucket_name,
-        Key = bucket_file_path)
+        Filename = FILE_NAME,
+        Bucket = BUCKET_NAME,
+        Key = BUCKET_FILE_PATH)
     
